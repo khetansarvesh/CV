717e10c2d16e14c7e495ff1273a757180170204c
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2024-07-01 06:12:38 +0000

    bug_fixes

--- representation_learning/residual_block_utils.py
+++ representation_learning/residual_block_utils.py
@@ -6,6 +6,8 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
+
+        # residual block
         self.block = nn.Sequential(
             nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True),
             nn.ReLU(),
@@ -14,12 +16,12 @@ class ResidualBlock(nn.Module):
             nn.ReLU(),
             nn.Conv2d(out_channels, out_channels, 3, 1, 1)
         )
-
-        if in_channels != out_channels:
-            self.channel_up = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
+        
+        # dimensionality matching block
+        self.dim_matching_layer = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
 
     def forward(self, x):
         if self.in_channels != self.out_channels:
-            return self.channel_up(x) + self.block(x)
+            return self.dim_matching_layer(x) + self.block(x)
         else:
             return x + self.block(x)

d760e91b38e1e1ed8751017d44cf8dfe2279cf4b
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2024-07-09 18:35:18 +0000

    code modularization

--- representation_learning/residual_block_utils.py
+++ representation_learning/residual_block_utils.py
@@ -2,15 +2,15 @@ import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, norm_num_groups = 32):
         super(ResidualBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
 
         # residual block
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1), nn.GroupNorm(num_groups=32, num_channels=out_channels, eps=1e-6, affine=True),nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1), nn.GroupNorm(num_groups=32, num_channels=out_channels, eps=1e-6, affine=True), nn.ReLU()
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1), nn.GroupNorm(num_groups=norm_num_groups, num_channels=out_channels, eps=1e-6, affine=True),nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1), nn.GroupNorm(num_groups=norm_num_groups, num_channels=out_channels, eps=1e-6, affine=True), nn.ReLU()
         )
         
         # dimensionality matching block

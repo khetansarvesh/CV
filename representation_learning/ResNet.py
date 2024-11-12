564c8aa0ecb255eec8d68e60113d0da5cd3e9d65
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2024-11-12 23:43:22 +0530

    Update ResNet.py

--- representation_learning/ResNet.py
+++ representation_learning/ResNet.py
@@ -94,13 +94,11 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
 
-def ResNet50(img_channel=3, num_classes=1000):
-    return ResNet(block, [3, 4, 6, 3], img_channel, num_classes)
+#ResNet50
+ResNet(block, [3, 4, 6, 3], 3, 1000)
 
+#ResNet101
+ResNet(block, [3, 4, 23, 3], 3, 1000)
 
-def ResNet101(img_channel=3, num_classes=1000):
-    return ResNet(block, [3, 4, 23, 3], img_channel, num_classes)
-
-
-def ResNet152(img_channel=3, num_classes=1000):
-    return ResNet(block, [3, 8, 36, 3], img_channel, num_classes)
+#ResNet152
+ResNet(block, [3, 8, 36, 3], 3, 1000)

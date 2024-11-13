75e10de34605fd91df912a21a193f788b2bfd571
Sarvesh Khetan <53877600+khetansarvesh@users.noreply.github.com>
2024-11-13 20:45:53 +0530

    Update ResNet.py

--- representation_learning/ResNet.py
+++ representation_learning/ResNet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from residual_block_utils.py import ResidualBlock2
 
 class ResNet(nn.Module):
+    
     def __init__(self):
         super(ResNet, self).__init__()
         self.in_channels = 64
@@ -10,10 +11,26 @@ class ResNet(nn.Module):
         self.conv1 = nn.Sequential( nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU() )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # Essentially the entire ResNet architecture are in these 4 lines below
-        self.layer1 = self._make_layer(3, intermediate_channels=64, stride=1)
-        self.layer2 = self._make_layer(8, intermediate_channels=128, stride=2)
+        self.layer1 = nn.Sequential(
+            ResidualBlock2(64, 256, nn.Sequential( nn.Conv2d(64, 256, kernel_size=1, stride=1, bias=False),nn.BatchNorm2d(256)), 1),
+            ResidualBlock2(256, 256),
+            ResidualBlock2(256, 256)
+        )
+        
+        self.layer2 = nn.Sequential(
+            ResidualBlock2(256, 512, nn.Sequential( nn.Conv2d( self.in_channels, 512, 1, 2, bias=False),nn.BatchNorm2d(512)), 2),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512),
+            ResidualBlock2(512, 512)
+        )
+        #self._make_layer(8, intermediate_channels=128, stride=2)
+        
         self.layer3 = self._make_layer(36, intermediate_channels=256, stride=2)
+        
         self.layer4 = self._make_layer(3, intermediate_channels=512, stride=2)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -24,12 +41,10 @@ class ResNet(nn.Module):
         # feature extraction
         x = self.conv1(x)
         x = self.maxpool(x)
-            
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
         x = self.avgpool(x)
             
         # image flattening
@@ -40,25 +55,17 @@ class ResNet(nn.Module):
 
         return x
 
-    def _make_layer(self, num_residual_blocks, intermediate_channels, stride):
+    def _make_layer(self, num_residual_blocks = 3, intermediate_channels=64, stride=1):
         layers = []
 
-        # Either if we half the input space for ex, 56x56 -> 28x28 (stride=2), or channels changes
-        # we need to adapt the Identity (skip connection) so it will be able to be added
-        # to the layer that's ahead
         if stride != 1 or self.in_channels != intermediate_channels * 4:
                 identity_downsample = nn.Sequential( nn.Conv2d( self.in_channels, intermediate_channels * 4, kernel_size=1, stride=stride, bias=False, ),nn.BatchNorm2d(intermediate_channels * 4))
         else:
                 identity_downsample = None
 
         layers.append(ResidualBlock2(self.in_channels, intermediate_channels, identity_downsample, stride))
-
-        # The expansion size is always 4 for ResNet 50,101,152
         self.in_channels = intermediate_channels * 4
 
-        # For example for first resnet layer: 256 will be mapped to 64 as intermediate layer,
-        # then finally back to 256. Hence no identity downsample is needed, since stride = 1,
-        # and also same amount of channels.
         for i in range(num_residual_blocks - 1):
             layers.append(ResidualBlock2(self.in_channels, intermediate_channels))
 
